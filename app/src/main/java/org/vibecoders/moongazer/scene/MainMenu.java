bed441c41c74b9fcc2a1201c136d6d8c345867e0
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-09-30 01:00:45 +0700

    feat(scene): implement skeleton main menu
    Doesn't have anything implemented inside yet, but we'll try.

---
+++ app/src/main/java/org/vibecoders/moongazer/scene/MainMenu.java
@@ -0,0 +1,19 @@
+package org.vibecoders.moongazer.scene;
+
+import static org.vibecoders.moongazer.Constants.*;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Main menu scene.
+ */
+public class MainMenu extends Scene {
+    /**
+     * Renders the main menu scene.
+     * @param batch The SpriteBatch to draw with.
+     */
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(TEXTURE_WHITE, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+}
