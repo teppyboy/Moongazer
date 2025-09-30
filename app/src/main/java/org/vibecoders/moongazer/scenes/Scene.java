ec1d2a672cf1279bc4d9cc7ce50a162e2520e717
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-09-30 08:05:20 +0700

    chore(scene): make the default constructor wait for all assets

--- app/src/main/java/org/vibecoders/moongazer/scenes/Scene.java
+++ app/src/main/java/org/vibecoders/moongazer/scenes/Scene.java
@@ -1,8 +1,15 @@
 package org.vibecoders.moongazer.scenes;
 
+import org.vibecoders.moongazer.managers.Assets;
+
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public abstract class Scene {
+    public Scene() {
+        if (!Assets.isLoadedAll()) {
+            Assets.waitUntilLoaded();
+        }
+    }
     public abstract void render(SpriteBatch batch);
     public void dispose() {
         // Default implementation does nothing
